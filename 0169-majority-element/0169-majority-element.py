0f6c2903358d068ce8cc49e7b8007973277de6cb
Prince Raj <pr663570@gmail.com>
2026-08-17 22:44:12 +0530

    Time: 6 ms (72.02%), Space: 13.7 MB (39.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        from collections import Counter
-        count_dict = Counter(nums)
-        
-        end_case = len(nums)//2
-        for key,value in count_dict.items():
-            if value > end_case:return key
+        count = 0 
+        uniq_num = 0
+        for curr_num in nums :
+            if count==0:
+                uniq_num=curr_num
+            if uniq_num==curr_num:
+                count+=1
+            else:
+                count-=1
+        return uniq_num
