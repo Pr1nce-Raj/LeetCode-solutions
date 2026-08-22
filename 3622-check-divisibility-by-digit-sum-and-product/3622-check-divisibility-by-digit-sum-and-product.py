420eb58580fc14f131c38cec0ab0456cd9f6a41b
Prince Raj <pr663570@gmail.com>
2026-08-22 22:38:54 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.2%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -5,12 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         original=n
-        dig_sum=0
-        dig_prod=1
+        arr=[0,1]
         while n>0:
-            dig_sum=dig_sum + (n%10)
-            dig_prod=dig_prod *(n%10)
+            arr[0]=arr[0] + (n%10)
+            arr[1]=arr[1] *(n%10)
             n=n/10
-        if original %(dig_sum+dig_prod)!=0:
+        if original %(arr[0]+arr[1])!=0:
             return False
         return True
